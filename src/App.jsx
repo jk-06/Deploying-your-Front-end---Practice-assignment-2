fa3a5eded50ba0438966d3407e7fbe95ef2f2252
Jebakumar <jebakumar.6208@gmail.com>
2025-03-04 10:27:06 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Jebakumar</h1>
+      <p>Contact: jebakumar@email.com</p>
+      <p>Phone: 9378787389</p>
+    </div>
+  );
+}
+
+export default App;
